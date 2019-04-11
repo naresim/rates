d23d0043139e3c2548adf32010cb8177483a9a61
Nare Simonyan <nare@narepicsart.picsart.int>
2019-04-11 15:16:47 +0400

    adding date and exchange rates

--- rates.py
+++ rates.py
@@ -1,9 +1,14 @@
 import requests, json
 from pprint import pprint
-print("Currency: ")
-base = input()
 
-result = requests.get("https://api.exchangeratesapi.io/latest?base=" + base )
+base = input("Currency: ")
+date = input("Date: ")
+rates = input("Exchange rates: ")
+
+#result = requests.get('https://api.exchangeratesapi.io/latest?base=' + base )
+link = 'https://api.exchangeratesapi.io/'
+url = link + 'history?start_at=' + date + '&end_at=' + date + '&symbols=' + rates + '&base=' + base
+result = requests.get(url)
 output = pprint(result.json())
 print (output)
 
